1935d55adb17eec63113b6b4e7d953f84f022155
Brian Rodriguez <sebastiaanbrian@outlook.com>
2022-10-27 21:27:52 +0200

    commit

--- PyVoIp_Doorbell/doorbell.py
+++ PyVoIp_Doorbell/doorbell.py
@@ -1,3 +1,23 @@
-import sys
 
-print(sys.path)
+import pyVoIP
+import socket
+import time
+import wave
+
+from pyVoIP.VoIP import VoIPPhone, CallState, InvalidStateError
+clientHostname = socket.gethostname
+localIP = socket.gethostbyaddr(clientHostname)
+sipSocket = "norabri.duckdns.org"
+
+def answer(call):
+    try:
+        call.answer()
+        call.hangup()
+    except InvalidStateError:
+        pass
+
+if __name__ == "__main__":
+    phone = VoIPPhone(sipSocket + 5062, "1000", "12345" + localIP, callCallback=answer)
+    phone.start()
+    input('Press any key to dissable conenction')
+    phone.stop()
